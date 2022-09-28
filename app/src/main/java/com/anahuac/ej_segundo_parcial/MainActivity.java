23e7bbb13d1ef0b9ad44baf75bf3387ac757eebf
brendamedina <bren1096@hotmail.com>
2022-09-27 21:14:34 -0500

    commit

---
+++ app/src/main/java/com/anahuac/ej_segundo_parcial/MainActivity.java
@@ -0,0 +1,29 @@
+package com.anahuac.ej_segundo_parcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnWidgets;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnWidgets = findViewById(R.id.btn_elements);
+        btnWidgets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WidgetsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
